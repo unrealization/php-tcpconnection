a3835fd2cf8f765655a7ba9687cc3dcd789c5eab
Dennis Wronka <dennis@notebook.dennis>
2024-06-21 14:51:22 +0200

    Fix version tag in PHPDocs.

--- src/TCPConnection.php
+++ src/TCPConnection.php
@@ -12,7 +12,7 @@ namespace unrealization;
  * @subpackage TCPConnection
  * @link http://php-classes.sourceforge.net/ PHP Class Collection
  * @author Dennis Wronka <reptiler@users.sourceforge.net>
- * @version 4.0.0
+ * @version 5.0.1
  * @license http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html LGPL 2.1
  */
 class TCPConnection
